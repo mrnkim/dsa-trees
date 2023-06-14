4293b2960625a7fb0f1f74bf1b4fd11255fff105
MKim <mrn.kim07@gmail.com>
2023-06-14 11:35:00 -0700

    finish sumValues()

--- tree.js
+++ tree.js
@@ -8,25 +8,38 @@ class TreeNode {
     this.children = children;
   }
 
+  check() {
+    console.log("check");
+  }
   /** sumValues(): add up all values of invoking node and its children.
    * Returns sum as an integer. */
   sumValues() {
+    let toVisitStack = [this];
 
+    let sum = 0;
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      sum += current.val;
+
+      for (let child of current.children) {
+        toVisitStack.push(child);
+      }
+    }
+
+    return sum;
   }
 
   /** countEvens(): starting from the invoking node and moving through its
    * children, count how many nodes have even values. Returns that count as
    * an integer. */
-  countEvens() {
-
-  }
+  countEvens() {}
 
   /** numGreater(lowerBound): starting from the invoking node and moving through
    * its children, return a count of the number of nodes whose value is greater
    * than lowerBound. */
-  numGreater(lowerBound){
-
-  }
+  numGreater(lowerBound) {}
 }
 
 class Tree {
@@ -36,19 +49,16 @@ class Tree {
 
   /** sumValues(): add up all values in the tree. */
   sumValues() {
-
+    if (!this.root) return 0;
+    return this.root.sumValues();
   }
 
   /** countEvens(): count all nodes in the tree that have even values. */
-  countEvens() {
-
-  }
+  countEvens() {}
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
-  numGreater(lowerBound) {
-
-  }
+  numGreater(lowerBound) {}
 }
 
 module.exports = { Tree, TreeNode };
